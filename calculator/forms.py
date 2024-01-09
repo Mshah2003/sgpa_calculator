f91f10dd93b9bf36f6079d1910b8ddc4d53ad023
Mshah2003 <123729708+Mshah2003@users.noreply.github.com>
2024-01-09 06:50:43 +0530

    Update forms.py

--- calculator/forms.py
+++ calculator/forms.py
@@ -16,14 +16,16 @@ class SGPAForm(forms.Form):
         ('F', 'F'),
     ]
 
-    EM = forms.ChoiceField(choices=GRADE_CHOICES, label="Engineering Mathematics")
-    BEE = forms.ChoiceField(choices=GRADE_CHOICES, label="Basic Electrical Engg / Basic Civil Engg")
-    EC = forms.ChoiceField(choices=GRADE_CHOICES, label="Engg Chemistry / Engg Physics")
-    BME = forms.ChoiceField(choices=GRADE_CHOICES, label="Basic Mechanical Engineering / PPS")
-    CS = forms.ChoiceField(choices=GRADE_CHOICES, label="Communication Skills / Human Values")
-    LL = forms.ChoiceField(choices=GRADE_CHOICES, label="Language Lab / Human Values Lab")
-    MPW = forms.ChoiceField(choices=GRADE_CHOICES, label="Manufacturing Practices Workshop / Computer prog. Lab")
-    CL = forms.ChoiceField(choices=GRADE_CHOICES, label="Chemistry Lab / Physics Lab")
-    BEEL = forms.ChoiceField(choices=GRADE_CHOICES, label="Basic Electrical Engineering Lab / Civil lab")
-    CAED = forms.ChoiceField(choices=GRADE_CHOICES, label="Computer Aided Machine Drawing / Engg graphics")
-    DECA = forms.ChoiceField(choices=GRADE_CHOICES, label="DECA")
+    CHOICE_SIZE = {'style': 'width: 200px; height: 30px;'}  # You can adjust the width and height as needed
+
+    EM = forms.ChoiceField(choices=GRADE_CHOICES, label="Engineering Mathematics", widget=forms.Select(attrs=CHOICE_SIZE))
+    BEE = forms.ChoiceField(choices=GRADE_CHOICES, label="Basic Electrical Engg / Basic Civil Engg", widget=forms.Select(attrs=CHOICE_SIZE))
+    EC = forms.ChoiceField(choices=GRADE_CHOICES, label="Engg Chemistry / Engg Physics", widget=forms.Select(attrs=CHOICE_SIZE))
+    BME = forms.ChoiceField(choices=GRADE_CHOICES, label="Basic Mechanical Engineering / PPS", widget=forms.Select(attrs=CHOICE_SIZE))
+    CS = forms.ChoiceField(choices=GRADE_CHOICES, label="Communication Skills / Human Values", widget=forms.Select(attrs=CHOICE_SIZE))
+    LL = forms.ChoiceField(choices=GRADE_CHOICES, label="Language Lab / Human Values Lab", widget=forms.Select(attrs=CHOICE_SIZE))
+    MPW = forms.ChoiceField(choices=GRADE_CHOICES, label="Manufacturing Practices Workshop / Computer prog. Lab", widget=forms.Select(attrs=CHOICE_SIZE))
+    CL = forms.ChoiceField(choices=GRADE_CHOICES, label="Chemistry Lab / Physics Lab", widget=forms.Select(attrs=CHOICE_SIZE))
+    BEEL = forms.ChoiceField(choices=GRADE_CHOICES, label="Basic Electrical Engineering Lab / Civil lab", widget=forms.Select(attrs=CHOICE_SIZE))
+    CAED = forms.ChoiceField(choices=GRADE_CHOICES, label="Computer Aided Machine Drawing / Engg graphics", widget=forms.Select(attrs=CHOICE_SIZE))
+    DECA = forms.ChoiceField(choices=GRADE_CHOICES, label="DECA", widget=forms.Select(attrs=CHOICE_SIZE))
