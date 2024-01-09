ba0bafa13348bd4608411419951c6fc6d8c0c346
Mshah2003 <123729708+Mshah2003@users.noreply.github.com>
2024-01-09 06:39:28 +0530

    Update forms.py

--- calculator/forms.py
+++ calculator/forms.py
@@ -1,14 +1,29 @@
 from django import forms
 
 class SGPAForm(forms.Form):
-    EM = forms.CharField(label="Engineering Mathematics")
-    BEE = forms.CharField(label="Basic Electrical Engg / Basic Civil Engg")
-    EC = forms.CharField(label="Engg Chemistry / Engg Physics")
-    BME = forms.CharField(label="Basic Mechanical Engineering / PPS")
-    CS = forms.CharField(label="Communication Skills / Human Values")
-    LL = forms.CharField(label="Language Lab / Human Values Lab")
-    MPW = forms.CharField(label="Manufacturing Practices Workshop / Computer prog. Lab")
-    CL = forms.CharField(label="Chemistry Lab / Physics Lab")
-    BEEL = forms.CharField(label="Basic Electrical Engineering Lab / Civil lab")
-    CAED = forms.CharField(label="Computer Aided Machine Drawing / Engg graphics")
-    DECA = forms.CharField(label="DECA")
+    GRADE_CHOICES = [
+        ('A++', 'A++'),
+        ('A+', 'A+'),
+        ('A', 'A'),
+        ('B+', 'B+'),
+        ('B', 'B'),
+        ('C+', 'C+'),
+        ('C', 'C'),
+        ('D+', 'D+'),
+        ('D', 'D'),
+        ('E+', 'E+'),
+        ('E', 'E'),
+        ('F', 'F'),
+    ]
+
+    EM = forms.ChoiceField(choices=GRADE_CHOICES, label="Engineering Mathematics")
+    BEE = forms.ChoiceField(choices=GRADE_CHOICES, label="Basic Electrical Engg / Basic Civil Engg")
+    EC = forms.ChoiceField(choices=GRADE_CHOICES, label="Engg Chemistry / Engg Physics")
+    BME = forms.ChoiceField(choices=GRADE_CHOICES, label="Basic Mechanical Engineering / PPS")
+    CS = forms.ChoiceField(choices=GRADE_CHOICES, label="Communication Skills / Human Values")
+    LL = forms.ChoiceField(choices=GRADE_CHOICES, label="Language Lab / Human Values Lab")
+    MPW = forms.ChoiceField(choices=GRADE_CHOICES, label="Manufacturing Practices Workshop / Computer prog. Lab")
+    CL = forms.ChoiceField(choices=GRADE_CHOICES, label="Chemistry Lab / Physics Lab")
+    BEEL = forms.ChoiceField(choices=GRADE_CHOICES, label="Basic Electrical Engineering Lab / Civil lab")
+    CAED = forms.ChoiceField(choices=GRADE_CHOICES, label="Computer Aided Machine Drawing / Engg graphics")
+    DECA = forms.ChoiceField(choices=GRADE_CHOICES, label="DECA")
